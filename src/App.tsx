0c6d1152be39afcacdbe732fc320f58c39234502
Florent Tuppo <florent.tuppo@gmail.com>
2023-12-27 15:07:39 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center text-gray-800 my-4">
+        Bienvenue sur ExploreNation
+      </h1>
+    </div>
+  );
+}
+
+export default App;
