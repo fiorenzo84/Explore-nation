3b686d5bc5b5fc416bcf9545931bfdd615b7de30
Florent Tuppo <florent.tuppo@gmail.com>
2024-01-02 18:33:20 +0100

    Implémentation des composants dans Home

--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -1,9 +1,36 @@
+import {useState, useEffect} from "react";
+import {NationSearch} from "../components/NationSearch";
+import {CountryList} from "../data/CountryList";
+import axios from "axios";
 
+interface Country {
+  cca3: string;
+  name: {
+    common: string;
+  };
+}
 
 export const Home = () => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [countries, setCountries] = useState<Country[]>([]); // Ajouté pour stocker la liste des pays
+
+  useEffect(() => {
+    axios
+      .get<Country[]>("https://restcountries.com/v3.1/all")
+      .then((response) => {
+        setCountries(response.data);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  const handleSearch = (term: string) => {
+    setSearchTerm(term);
+  };
+
   return (
-    <div className="m-6 h-auto top-16 absolute bg-red-400">
-       recherhe <br />bouton slide <br />la map <br />le resultat de recherche
-    </div>
-  )
-}
+    <>
+      <NationSearch onSearch={handleSearch} countries={countries} />
+      <CountryList searchTerm={searchTerm} />
+    </>
+  );
+};
