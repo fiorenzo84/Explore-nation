b4ebb358870ecfc275c02e504e42da9d6b32aa1c
Florent Tuppo <florent.tuppo@gmail.com>
2024-01-02 18:28:54 +0100

    Personnalisation de certain éléments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}", // Ajoutez tous les chemins vers vos fichiers React
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom:
+          "0 0px 0px 0px rgba(90, 200, 126, 0.7), 0 4px 3px 0px rgba(90, 200, 126, 0.5)", //ombre personnalisée
+      },
+      colors: {
+        customGreen: "#5AC87E",
+      },
+      maxWidth: {
+        customWidth: "768px",
+      },
+    },
   },
   plugins: [],
 };
